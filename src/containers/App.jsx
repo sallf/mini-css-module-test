38e41863e98fcbd11a8d8aa2ab7dc16e269cb149
Saylor Allf <online@sallf.com>
2020-08-07 07:17:12 -0600

    Initial commit

---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <p>The universe is a big place, perhaps the biggest.</p>
+    )
+  }
+}
+
+export default App;
